e5f21237cec87859630613a29e221c7c88379d4c
Abhinavbhar <111721622+Abhinavbhar@users.noreply.github.com>
2024-01-20 11:40:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const express = require('express')
+const app = express()
+app.post('/login', (req, res) => {
+	res.send('Hello, you are on the login route')
+})
+
+app.post('/signup', (req, res) => {
+	res.send('Hello, you are on the signup route')
+})
+
+app.listen(1338, () => {
+	console.log(`Server is running on port 1338`)
+})
+app.get('/', (req, res) => {
+	res.send('your server  is running')
+})
